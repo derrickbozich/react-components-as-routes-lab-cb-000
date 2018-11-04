46b540f9e2b6135f3798e8d041fb00151eb64a5d
derrick b <dbozich1@gmail.com>
2018-11-04 03:49:47 +0000

    automatically backed up by learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorsToRender = directors.map(director => <div>{director.name}  {director.movies.join(' ')} </div> )
+
   return (
     <div>
       <h1>Directors Page</h1>
