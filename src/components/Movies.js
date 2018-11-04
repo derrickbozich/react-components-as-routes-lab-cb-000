9d9da8087c3e4ccb549eeb99de78bdfafc88dee7
derrick b <dbozich1@gmail.com>
2018-11-04 03:46:19 +0000

    automatically backed up by learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-  const moviesToRender = movies.map(movie => <div>{movie.title} {movie.time} {movie.genres}</div> )
+  const moviesToRender = movies.map(movie => <div>{movie.title} {movie.time} {...movie.genres}</div> )
   return (
     <div>
       <h1>Movies Page</h1>
