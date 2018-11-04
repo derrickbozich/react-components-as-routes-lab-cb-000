54f70ab0f181aa1da472dbfab509e9f05a3cbaae
derrick b <dbozich1@gmail.com>
2018-11-04 03:48:45 +0000

    automatically backed up by learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const actorsToRender = actors.map(actor => <div>{actor.name}  {actor.movies.join(' ')} </div> )
+
   return (
     <div>
       <h1>Actors Page</h1>
